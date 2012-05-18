7531c8f968e84fcbb1a7cea675a3844a109797fa
wwq0327 <wwq0327@gmail.com>
2012-05-18 23:50:09 +0800

    f

--- openclass/media/js/class.js
+++ openclass/media/js/class.js
@@ -1,1 +1,34 @@
-$('.dropdown-toggle').dropdown();
+$('html').ajaxSend(function(event, xhr, settings) {
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+        // Only send the token to relative URLs i.e. locally.
+        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+    }
+});
+
+
+//$(".dropdown-toggle").dropdown();
+
+$(document).ready(function() {
+    $("form.form-horizontal").submit(function() {
+        var comment = $("#id_comment").val();
+        if (!comment || comment.length < 4) {
+            alert("回复不能为空或少于4字符！");
+            return false;
+        }
+    });
+});
