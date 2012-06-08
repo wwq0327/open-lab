d2112c8d3b0303f31ed044f2f6fad21077d4b597
wwq0327 <wwq0327@gmail.com>
2012-06-08 19:44:32 +0800

    首页模板修改，登录与未登录分别显示不同的状态

--- openclass/apps/home/views.py
+++ openclass/apps/home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from events.models import Event
 
+#@login_required
 def index(request):
+    es = Event.objects.all()
+
     return render_to_response('home/index.html',
-                              {},
+                              {'events': es},
                               context_instance=RequestContext(request))
