76a432a72c20e4892562492745c8a7b9e0b273ce
wwq0327 <wwq0327@gmail.com>
2012-06-09 17:38:13 +0800

    首页动态显示功能

--- openclass/apps/home/views.py
+++ openclass/apps/home/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from events.models import Event
-
+from projects.models import PrjFollower
 #@login_required
 def index(request):
     es = Event.objects.all()
+    #u = User.objects.get(username=request.user.username)
+    if request.user.is_authenticated():
+        count = PrjFollower.objects.filter(follower=request.user).count()
+    else:
+        count = None
 
     return render_to_response('home/index.html',
-                              {'events': es},
+                              {'events': es,
+                               'get_follow': count},
                               context_instance=RequestContext(request))
